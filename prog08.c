2ad6586402d0b9cc79ae93ce368357ad2f3da4b4
Eliane Orlandin <liorlandin33@gmail.com>
2025-02-10 20:29:54 -0300

    Variables

---
+++ prog08.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int a;
+
+/*
+int faz_nada(){
+    int c;
+    return c;
+}
+*/
+
+int main()
+{
+    int b;
+    printf("a: %d\nb: %d\n", a, b);
+    // printf("%d\n%d\n%d\n", a, b, faz_nada());
+
+    return 0;
+}
