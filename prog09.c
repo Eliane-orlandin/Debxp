40fa1620700c07dd7376e260585d3a1c5930dad4
Eliane Orlandin <liorlandin33@gmail.com>
2025-02-12 20:54:37 -0300

    Pointer

--- prog09.c
+++ prog09.c
@@ -3,9 +3,12 @@
 int main()
 {
     int valor = 10;
+    int *pvalor = &valor;
 
-    printf("%d\n", valor);
-    printf("%p\n", &valor);
+    printf("%d\n", valor);   // Imprime 10
+    printf("%d\n", *pvalor); // Imprime 10
+    printf("%p\n", &valor);  // Imprime o endereço
+    printf("%p\n", pvalor);  // Imprime o endereço
 
     return 0;
 }
