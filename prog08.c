e32d46d7b5a04ad49e5c099ec3ec64ea08afd83e
Eliane Orlandin <liorlandin33@gmail.com>
2025-02-11 21:23:50 -0300

    Variables

--- prog08.c
+++ prog08.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int a;
+int a = 5;
+int c = 45;
 
-/*
-int faz_nada(){
-    int c;
+int faz_nada()
+{
+    int c = 15;
     return c;
 }
-*/
 
 int main()
 {
-    int b;
-    printf("a: %d\nb: %d\n", a, b);
-    // printf("%d\n%d\n%d\n", a, b, faz_nada());
+    int b = 10;
+    // printf("a: %d\nb: %d\n", a, b);
+    printf("%d\n%d\n%d\n", a, b, faz_nada());
 
     return 0;
 }
