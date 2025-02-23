57ed815bf709272a8d0655d161750e017c05e6a2
Eliane Orlandin <liorlandin33@gmail.com>
2025-02-22 22:12:53 -0300

    Declaration of a Pointer of Pointers

---
+++ prog42.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10;
+    int *p = &x;   // Ponteiro para x
+    int **pp = &p; // Ponteiro para o ponteiro p
+
+    printf("Valor de x        : %d\n", x);    // 10
+    printf("Valor de x via p  : %d\n", *p);   // 10
+    printf("Valor de x via pp : %d\n", **pp); // 10
+
+    return 0;
+}
+/*
+Explicação:
+x → Variável contendo 10
+p → Aponta para x, então *p retorna 10
+pp → Aponta para p, então **pp retorna 10
+*pp retorna o endereço de p
+**pp retorna o valor de x
+*/
